ee21c097c1fc83471c506726b93eca38848a9731
rockyindian <46916347+rockyindian@users.noreply.github.com>
2019-05-08 10:36:34 -0500

    first coomit

---
+++ multiArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] i = new int[ ] {{3, 4},{1, 0},{2, 1}};
+            int b,j;
+            for (b = 0; b < 10; b++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(i[b,j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
